a7ca384ffc0a62d53430044152f11f73bc5a3c14
bluenote-1577 <jimshawster@gmail.com>
2024-07-25 16:11:07 -0400

    added imgvr/fungi reps + viral host annotation + concatenation of metadata files. reorganized folder structure

--- sylph_to_taxprof.py
+++ sylph_to_taxprof.py
@@ -1,20 +1,27 @@
-import pandas as pd
-from collections import defaultdict
-import sys
-import csv
-import gzip
+import pandas as pd 
+from collections import defaultdict 
+import sys 
+import csv 
+import gzip 
 import argparse
 
-def genome_file_to_gtdb_acc(file_name):
+def genome_file_to_gcf_acc(file_name):
+    if 'ASM' in file_name:
+        return file_name.split('/')[-1].split('_ASM')[0]
     return(file_name.split('/')[-1].split("_genomic")[0])
+
+def contig_to_imgvr_acc(contig_name):
+    return contig_name.split(' ')[0].split('|')[0]
 
 parser = argparse.ArgumentParser(description='Convert a sylph output tsv file to a set of output files in MetaPhlAn format.')
 parser.add_argument("-s", "--sylph", help="sylph output file", type=str, required=True)
-parser.add_argument("-o", "--output-prefix", help="prefix of the outputs. Output files will be prefix + Sample_file +.sylphmpa", type=str, default = "")
-parser.add_argument("-m", "--metadata", help = "metadata file converting genome files to taxnomic identifiers.", type = str, required=True) 
+parser.add_argument("-o", "--output-prefix", help="prefix of the outputs. Output files will be 'prefix + Sample_file_column + .sylphmpa'", type=str, default = "")
+parser.add_argument("-m", "--metadata", help = "metadata file(s) converting genome files to taxonomic identifiers. Multiple input files will be concatenated.", type = str, required=True, nargs='+')
+parser.add_argument("-a", "--annotate-virus-hosts", help = "add additional column(s) by integrating viral-host information available (currently available for IMGVR4.1)", action='store_true')
 #add versioning
-parser.add_argument('--version', action='version', version='%(prog)s 0.1')
+parser.add_argument('--version', action='version', version='%(prog)s 0.2')
 args = parser.parse_args()
+annotate_virus = args.annotate_virus_hosts
 
 # Read sylph's output TSV file into a Pandas DataFrame
 df = pd.read_csv(args.sylph, sep='\t')
@@ -23,46 +30,68 @@ df = pd.read_csv(args.sylph, sep='\t')
 ### to taxonomy string mapping. It should be like
 ### {'my_genome.fna.gz' : b__Bacteria;...}
 genome_to_taxonomy = dict()
+genome_to_additional_data = dict()
 
-### Process gzip file instead if extension detected
-if '.gz' in args.metadata or '.gzip' in args.metadata:
-    f=gzip.open(args.metadata,'rt')
-else:
-    f = open(args.metadata,'r')
+print(f"Reading metadata files: {args.metadata} ...")
 
-### Tag each taxonomy string with a t__ strain level identifier
-for row in f:
-    spl = row.rstrip().split();
-    accession = spl[0]
-    taxonomy = ' '.join(spl[1:]).rstrip() + ';t__' + accession
-    genome_to_taxonomy[accession] = taxonomy
+for file in args.metadata:
+    ### Process gzip file instead if extension detected
+    if '.gz' in file or '.gzip' in file:
+        f=gzip.open(file,'rt')
+    else:
+        f = open(file,'r')
+
+    ### Tag each taxonomy string with a t__ strain level identifier
+    for row in f:
+        spl = row.rstrip().split('\t');
+        accession = spl[0]
+        taxonomy = spl[1].rstrip() + ';t__' + accession
+        if len(spl) > 2:
+            genome_to_additional_data[accession] = spl[2].rstrip()
+        genome_to_taxonomy[accession] = taxonomy
 
 ### Group by sample file. Output one file fo reach sample file. 
+print(f"Processing sylph output file: {args.sylph} ...")
 grouped = df.groupby('Sample_file')
 
 for sample_file, group_df in grouped:
+    first_warn = False
     out = sample_file.split('/')[-1]
     out_file = args.output_prefix + out + '.sylphmpa'
     of = open(out_file,'w')
+    print(f"Writing output to: {out_file} ...")
 
     tax_abundance = defaultdict(float)
     seq_abundance = defaultdict(float)
     ani_dict = defaultdict(float)
+    cov_dict = defaultdict(float)
 
     # Iterate over each row in the DataFrame
     for idx, row in group_df.iterrows():
+        if 'Genome_file' not in row or 'Contig_name' not in row or 'Adjusted_ANI' not in row or 'Sequence_abundance' not in row:
+            print("ERROR: Missing required columns in sylph output file. Exiting.")
+            sys.exit(1)
 
         # Parse the genome file... assume the file is in gtdb format.
         # This can be changed. 
-        genome_file = genome_file_to_gtdb_acc(row['Genome_file'])
+        genome_file = genome_file_to_gcf_acc(row['Genome_file'])
+        contig_id = contig_to_imgvr_acc(row['Contig_name'])
         ani = float(row['Adjusted_ANI'])
+        if 'Eff_cov' in row:
+            cov = float(row['Eff_cov'])
+        else:
+            cov = float(row['True_cov'])
 
         if genome_file in genome_to_taxonomy:
             tax_str = genome_to_taxonomy[genome_file]
         elif genome_file +'.gz' in genome_to_taxonomy:
             tax_str = genome_to_taxonomy[genome_file + '.gz']
+        elif contig_id in genome_to_taxonomy:
+            tax_str = genome_to_taxonomy[contig_id]
         else:
             tax_str = 'NO_TAXONOMY;t__' + genome_file
+            if not first_warn:
+                print(f"WARNING: No taxonomy information found for entry {genome_file} and contig {contig_id} in metadata files. Assigning default taxonomy and skipping future warnings.")
 
         abundance = float(row['Sequence_abundance'])
         rel_abundance = float(row['Taxonomic_abundance'])
@@ -78,10 +107,15 @@ for sample_file, group_df in grouped:
             seq_abundance[cur_tax] += abundance
             if 't__' in cur_tax:
                 ani_dict[cur_tax] = ani
+            if 't__' in cur_tax:
+                cov_dict[cur_tax] = cov
 
     # Print the CAMI BioBoxes profiling format
     of.write(f"#SampleID\t{sample_file}\n")
-    of.write("clade_name\trelative_abundance\tsequence_abundance\tANI (if strain-level)\n")
+    if annotate_virus:
+        of.write("clade_name\trelative_abundance\tsequence_abundance\tANI (if strain-level)\t Coverage (if strain-level)\tVirus_host (if viral)\n")
+    else:
+        of.write("clade_name\trelative_abundance\tsequence_abundance\tANI (if strain-level)\t Coverage (if strain-level)\n")
 
     level_to_key = dict()
     for key in tax_abundance.keys():
@@ -97,7 +131,18 @@ for sample_file, group_df in grouped:
         keys_for_level = sorted(level_to_key[level], key = lambda x: tax_abundance[x], reverse=True)
         for tax in keys_for_level:
             if tax in ani_dict:
-                of.write(f"{tax}\t{tax_abundance[tax]}\t{seq_abundance[tax]}\t{ani_dict[tax]}\n")
+                if annotate_virus:
+                    accession = tax.split('t__')[-1]
+                    if accession in genome_to_additional_data:
+                        val = genome_to_additional_data[accession]
+                    else:
+                        val = 'NA'
+                    of.write(f"{tax}\t{tax_abundance[tax]}\t{seq_abundance[tax]}\t{ani_dict[tax]}\t{cov_dict[tax]}\t{val}\n")
+                else:
+                    of.write(f"{tax}\t{tax_abundance[tax]}\t{seq_abundance[tax]}\t{ani_dict[tax]}\t{cov_dict[tax]}\n")
             else:
-                of.write(f"{tax}\t{tax_abundance[tax]}\t{seq_abundance[tax]}\tNA\n")
+                if annotate_virus:
+                    of.write(f"{tax}\t{tax_abundance[tax]}\t{seq_abundance[tax]}\tNA\tNA\tNA\n")
+                else:
+                    of.write(f"{tax}\t{tax_abundance[tax]}\t{seq_abundance[tax]}\tNA\tNA\n")
 
